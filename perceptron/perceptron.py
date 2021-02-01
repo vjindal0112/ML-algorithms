52a656638601e4421c32c751fc8cc7b4a17cef97
Varun Jindal <vjindal0112@gmail.com>
2021-01-31 20:45:16 -0500

    initial commit

---
+++ perceptron/perceptron.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+
+X = np.array([
+        [3, 3],
+        [7,3],
+        [-5,2],
+        [-1,-1],
+        [-1,1]
+    ])
+Y = np.array([1,1,-1,-1, -1])
+
+theta = [0,0]
+
+misclassified = True
+k = 0
+while (misclassified):
+    misclassified = False
+    shuffler = np.random.permutation(len(X))
+    Y = Y[shuffler]
+    X = X[shuffler]
+    for x in range(len(X)):
+        if ((Y[x] * np.dot(X[x], theta)) <= 0):
+            theta += np.multiply(Y[x], X[x])
+            print(k)
+            print(theta)
+            k += 1
+            misclassified = True
+
+print()
+print("ANSWER:")
+print(theta)
+print("k=" + str(k))
